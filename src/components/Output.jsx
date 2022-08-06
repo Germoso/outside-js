9381b8499bf1b89d140b835a778a3c32afa4f983
Alexander <mailonywilo@gmail.com>
2022-08-06 10:37:22 -0400

    autorun

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,23 +1,15 @@
 import React, { useContext } from "react"
-import { context, INITIAL_STATE } from "../context/OutsideJs"
+import { context } from "../context/OutsideJs"
 import { Errors } from "./Errors"
 import { Logs } from "./Logs"
-import { ClearButton } from "./ClearButtons"
 
 const Output = () => {
-    const [messages, setMessages] = useContext(context)
-
-    const clearOutput = () => {
-        setMessages(INITIAL_STATE)
-    }
+    const [messages] = useContext(context)
 
     return (
         <div className="relative grow text-paragraph  flex flex-col">
             <Logs messages={messages} />
             <Errors messages={messages} />
-            <div className="absolute top-2 right-2">
-                <ClearButton text="clear console" callback={clearOutput} />
-            </div>
         </div>
     )
 }
--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,43 +1,57 @@
 import Editor from "@monaco-editor/react"
-import React, { useState } from "react"
+import React, { useState, useContext, useEffect, useCallback } from "react"
 import { Loading } from "./Loading"
 import { useConsole } from "../hooks/useConsole"
 import { useTranspiler } from "../hooks/useTranspiler"
+import { context, INITIAL_STATE } from "../context/OutsideJs"
 
 const Playground = () => {
     const [untranspiledCode, setUntranspiledCode] = useState("")
     const { code } = useTranspiler(untranspiledCode)
     const outside_js = useConsole()
+    const [, setMessages] = useContext(context)
 
-    //NUEVO CODIGO
-
-    /* require monaco */
+    const clearOutput = useCallback(() => {
+        setMessages(INITIAL_STATE)
+    }, [setMessages])
 
     const handleChange = (value, e) => {
         setUntranspiledCode(value)
+        // setTimeout(() => {
+        //     clearOutput()
+        //     run()
+        // }, 1000)
     }
 
-    const run = () => {
+    const run = useCallback(() => {
         try {
             // eslint-disable-next-line no-eval
             eval(code)
         } catch (error) {
             outside_js.error(error)
         }
-    }
+    }, [code, outside_js])
+
+    useEffect(() => {
+        setTimeout(() => {
+            clearOutput()
+            run()
+        }, 1)
+    }, [code, clearOutput, run])
 
     return (
         <div className="relative w-3/5 border-r border-paragraph pt-2">
             <Editor
+                // ref={editorRef}
                 defaultLanguage="javascript"
                 onChange={handleChange}
-                loading={Loading}
+                loading={<Loading />}
                 theme={"vs-dark"}
             />
             <div className=" flex w-full justify-center absolute bottom-0">
                 <button
                     className=" select-none bottom-2 border border-headlines left-1/2 text-headline py-2 px-4 hover:text-background hover:bg-headline transition-colors my-2"
-                    onClick={run}
+                    // onClick={run}
                 >
                     Run
                 </button>
